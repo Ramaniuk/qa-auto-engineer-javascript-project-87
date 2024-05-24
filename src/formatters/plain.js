bf46fd26a22e1ad6ed0b97ddf2b7fca8668902f0
Ramaniuk <240724oi@gmail.com>
2024-05-23 23:33:09 -0400

    fixed eslint

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,21 +1,19 @@
 const plain = (str) => {
   const arr = str.split('\n');
-  const result = [];
+  let result = [];
   const obj = {};
   arr.forEach((el) => {
-    const array = el.split(': ');
-    const key = array[0].slice(4);
-    const value = array[1];
-
-    if ((el.includes('-') || el.includes('+')) && Object.hasOwn(obj, key)) {
-      result.pop();
-      result.push(`Property '${key}' was updated. From ${obj[key]} to ${value}`);
+    const [key, value] = el.split(': ');
+    const slicedKey = key.slice(4);
+    if ((el.includes('-') || el.includes('+')) && Object.hasOwn(obj, slicedKey)) {
+      result = [...result.slice(0, -1), `Property '${slicedKey}' was updated. From ${obj[slicedKey]} to ${value}`];
     } else if (el.includes('-')) {
-      result.push(`Property '${key}' was removed`);
+      result = [...result, `Property '${slicedKey}' was removed`];
+      console.log(result)
     } else if (el.includes('+')) {
-      result.push(`Property '${key}' was added with value: ${value}`);
+      result = [...result,`Property '${slicedKey}' was added with value: ${value}`];
     }
-    obj[key] = value;
+    obj[slicedKey] = value;
   });
   return result.join('\n');
 };
