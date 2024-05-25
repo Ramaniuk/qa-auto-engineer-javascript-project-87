ff05547ea34c125122ea8e73784d2579e6582426
Ramaniuk <240724oi@gmail.com>
2024-05-25 00:19:32 -0400

    fixed eslint

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,13 +1,13 @@
 const plain = (str) => {
   const arr = str.split('\n');
 
-  const result = arr.reduce((acc, el, index, arr) => {
+  const result = arr.reduce((acc, el, index, array) => {
     const [key, value] = el.split(': ');
     const slicedKey = key.slice(4);
-    if ((acc.includes(`Property '${slicedKey}' was removed`) 
+    if ((acc.includes(`Property '${slicedKey}' was removed`)
       || acc.includes(`Property '${slicedKey}' was added with value: ${value}`))
     ) {
-      const [, oldValue] = arr[index - 1].split(': ');
+      const [, oldValue] = array[index - 1].split(': ');
       return [...acc.slice(0, -1), `Property '${slicedKey}' was updated. From ${oldValue} to ${value}`];
     } if (el.includes('-')) {
       return [...acc, `Property '${slicedKey}' was removed`];
