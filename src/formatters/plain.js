216913fd1b30ac1284bbae7d6aab339946cb071e
Ramaniuk <240724oi@gmail.com>
2024-05-25 00:17:18 -0400

    fixed eslint

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,19 +1,17 @@
 const plain = (str) => {
   const arr = str.split('\n');
-  const obj = {};
 
-  const result = arr.reduce((acc, el) => {
+  const result = arr.reduce((acc, el, index, arr) => {
     const [key, value] = el.split(': ');
     const slicedKey = key.slice(4);
-    if ((el.includes('-') || el.includes('+')) && Object.hasOwn(obj, slicedKey)) {
-      const oldValue = obj[slicedKey];
-      obj[slicedKey] = value;
+    if ((acc.includes(`Property '${slicedKey}' was removed`) 
+      || acc.includes(`Property '${slicedKey}' was added with value: ${value}`))
+    ) {
+      const [, oldValue] = arr[index - 1].split(': ');
       return [...acc.slice(0, -1), `Property '${slicedKey}' was updated. From ${oldValue} to ${value}`];
-    } else if (el.includes('-')) {
-      obj[slicedKey] = value;
+    } if (el.includes('-')) {
       return [...acc, `Property '${slicedKey}' was removed`];
-    } else if (el.includes('+')) {
-      obj[slicedKey] = value;
+    } if (el.includes('+')) {
       return [...acc, `Property '${slicedKey}' was added with value: ${value}`];
     }
     return acc;
