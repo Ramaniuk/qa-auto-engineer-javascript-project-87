18090f60ca39613027b9c6a1875ff5b03637eed4
Ramaniuk <240724oi@gmail.com>
2024-05-24 23:51:57 -0400

    fixed eslint

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -6,13 +6,16 @@ const plain = (str) => {
     const [key, value] = el.split(': ');
     const slicedKey = key.slice(4);
     if ((el.includes('-') || el.includes('+')) && Object.hasOwn(obj, slicedKey)) {
-      acc = [...acc.slice(0, -1), `Property '${slicedKey}' was updated. From ${obj[slicedKey]} to ${value}`];
+      const oldValue = obj[slicedKey];
+      obj[slicedKey] = value;
+      return [...acc.slice(0, -1), `Property '${slicedKey}' was updated. From ${oldValue} to ${value}`];
     } else if (el.includes('-')) {
-      acc = [...acc, `Property '${slicedKey}' was removed`];
+      obj[slicedKey] = value;
+      return [...acc, `Property '${slicedKey}' was removed`];
     } else if (el.includes('+')) {
-      acc = [...acc, `Property '${slicedKey}' was added with value: ${value}`];
+      obj[slicedKey] = value;
+      return [...acc, `Property '${slicedKey}' was added with value: ${value}`];
     }
-    obj[slicedKey] = value;
     return acc;
   }, []).join('\n');
   return result;
